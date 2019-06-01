385b8b2d95901efcebadae4dd6b90bb07122fcb5
briantam23 <briantam23@gmail.com>
2019-06-01 10:53:07 -0400

    Added a few functions

--- index.js
+++ index.js
@@ -26,3 +26,42 @@ const printBoard = () => {
         ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n'
     );
 }
+
+const validateMove = position => {
+    if(Number.isInteger(parseFloat(position)) === true && board[position] === ' ') {
+        return true;
+    }
+    return false;
+}
+
+// Everyone possible combination of three in a row
+const winCombinations = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+];
+
+// Determins if the passed in player has three in a row
+const checkWin = player => {
+    for(let i = 0; i < winCombinations.length; i++) {
+        let markCount = 0;
+        for(let j = 0; j < winCombinations[i].length; j++) {
+            if(board[winCombinations[i][j]] === player) {
+                markCount++;
+            }
+            if(markCount === 3) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const checkDraw = () => {
+    let markCount = 0;
+    for(const position in board) {
+        if(board[position] === ' ') {
+            return false;
+        }
+    }
+    return true;
+}
